1f78179da738d1ba507e7aea576e5803114d8fb8
Simon Rönnqvist <simon@iki.fi>
2019-01-11 20:32:19 +0200

    t.text :priority, null: false, default: 'LOW' + db:migrate

--- db/migrate/20190111182636_create_announcements.rb
+++ db/migrate/20190111182636_create_announcements.rb
@@ -2,7 +2,7 @@ class CreateAnnouncements < ActiveRecord::Migration[5.2]
   def change
     create_table :announcements do |t|
       t.text :type
-      t.text :priority
+      t.text :priority, null: false, default: 'LOW'
 
       t.timestamps
     end
