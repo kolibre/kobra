c2e13fbea89efc17659d8239a47e817183b5f58b
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-20 13:35:14 +0200

    enforce not null

--- db/migrate/20190220113222_create_announcement_texts.rb
+++ db/migrate/20190220113222_create_announcement_texts.rb
@@ -2,7 +2,7 @@ class CreateAnnouncementTexts < ActiveRecord::Migration[5.2]
   def change
     create_table :announcement_texts do |t|
       t.references :announcement, foreign_key: true
-      t.text :text
+      t.text :text, null: false
       t.references :language, foreign_key: true
 
       t.timestamps
