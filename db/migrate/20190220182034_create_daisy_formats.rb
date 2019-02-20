cd61458ea5be34d3c2457fbb2098620a0fb7155d
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-20 20:21:45 +0200

    enforce not null

--- db/migrate/20190220182034_create_daisy_formats.rb
+++ db/migrate/20190220182034_create_daisy_formats.rb
@@ -1,7 +1,7 @@
 class CreateDaisyFormats < ActiveRecord::Migration[5.2]
   def change
     create_table :daisy_formats do |t|
-      t.text :format
+      t.text :format, null: false
 
       t.timestamps
     end
