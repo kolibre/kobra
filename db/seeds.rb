bc9dfc8a17981efba939989c9664b284ec6a9c15
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-21 19:00:29 +0200

    seed database with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ name: 'BOOK' }, { name: 'MAGAZINE' }, { name: 'NEWSPAPER' }, { name: 'OTHER' }])
