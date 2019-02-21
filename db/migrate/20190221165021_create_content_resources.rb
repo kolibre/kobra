b276bd32b9e094d32880bbb6d0795824407e617e
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-21 18:52:18 +0200

    enforce not null and set defaults

--- db/migrate/20190221165021_create_content_resources.rb
+++ db/migrate/20190221165021_create_content_resources.rb
@@ -2,9 +2,9 @@ class CreateContentResources < ActiveRecord::Migration[5.2]
   def change
     create_table :content_resources do |t|
       t.references :content, foreign_key: true
-      t.text :filename
-      t.integer :bytes
-      t.text :mimetype
+      t.text :filename, null: false, default: ''
+      t.integer :bytes, null: false, default: 0
+      t.text :mimetype, null: false, default: 'unknown'
 
       t.timestamps
     end
