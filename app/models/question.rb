7f75fe5334423cfb25eac622bd3bb53ad12eb3d2
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-21 19:49:49 +0200

    add parent reference

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :question_type
+  belongs_to :parent, :class_name => 'Question'
+  has_many :children, :class_name => 'Question', :foreign_key => 'parent_id'
 end
--- db/migrate/20190221173404_create_questions.rb
+++ db/migrate/20190221173404_create_questions.rb
@@ -1,6 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
+      t.references :parent, foreign_key: true
       t.references :question_type, foreign_key: true
 
       t.timestamps
