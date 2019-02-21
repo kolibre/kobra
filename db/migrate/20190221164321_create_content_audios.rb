7fb028f2d771fedd6c8f7766a6a34a0be0fcfcd7
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-21 18:45:14 +0200

    enforce not null

--- db/migrate/20190221164321_create_content_audios.rb
+++ db/migrate/20190221164321_create_content_audios.rb
@@ -2,8 +2,8 @@ class CreateContentAudios < ActiveRecord::Migration[5.2]
   def change
     create_table :content_audios do |t|
       t.references :content, foreign_key: true
-      t.integer :size
-      t.integer :length
+      t.integer :size, null: false
+      t.integer :length, null: false
 
       t.timestamps
     end
