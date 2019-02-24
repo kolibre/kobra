93e8043b8cd07e2db6adad6769b9641e9fe93c5d
Johannes Skogman <johannes.skogman@gmail.com>
2019-02-24 12:58:23 +0200

    rename variables for consistensy, add forgotten null constraints/foreign_keys

--- db/migrate/20190220182555_create_contents.rb
+++ db/migrate/20190220182555_create_contents.rb
@@ -2,9 +2,8 @@ class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
       t.references :category, foreign_key: true
-      t.references :daisyformat, foreign_key: true
+      t.references :daisy_format, foreign_key: true
       t.text :title, null: false
-      t.numeric :date, null: false
 
       t.timestamps
     end
--- db/migrate/20190220183617_create_user_bookmarks.rb
+++ db/migrate/20190220183617_create_user_bookmarks.rb
@@ -2,9 +2,8 @@ class CreateUserBookmarks < ActiveRecord::Migration[5.2]
   def change
     create_table :user_bookmarks do |t|
       t.references :user, foreign_key: true
-      # content is not a foreign key in the KADOS example database. A mistake perhaps?
       t.references :content, foreign_key: true
-      t.text :bookmarkset, null: false
+      t.text :bookmark_set, null: false
 
       t.timestamps
     end
