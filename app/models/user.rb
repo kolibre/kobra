fe1b55ed8ec0b4ef21eef35396f762969802576a
Johannes Skogman <johannes.skogman@gmail.com>
2019-03-26 12:44:02 +0200

    add username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 require 'sysrandom'
 class User < ApplicationRecord
   before_save :generate_password
+  validates :username, uniqueness: true
   def generate_password
-    self.password ||= Sysrandom.base64(8)
+    self.password = Sysrandom.base64(9) if password.nil? || password.empty?
   end
 end
