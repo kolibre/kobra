9043413f3423ad29f25b1c710932ff86a3f8bc85
Johannes Skogman <johannes.skogman@gmail.com>
2019-06-30 16:24:14 +0300

    Specify folder path for audio uploads

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -10,7 +10,7 @@ class AudioUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{ENV['STORAGE_PATH']}/#{model.content.id}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
