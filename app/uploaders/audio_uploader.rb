950e1c872fc46165e0d3e5e7c346a8c7ef8a2e26
Johannes Skogman <johannes.skogman@gmail.com>
2019-07-02 17:55:50 +0300

    Add renaming function to audio uploader

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -41,7 +41,7 @@ class AudioUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "content.#{model.audio.file.extension}" if original_filename
+  end
 end
