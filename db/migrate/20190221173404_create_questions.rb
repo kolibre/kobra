01164afe2e65118b521529e1218ad37e6af5e338
Johan Abbors <jabbors@gmail.com>
2019-11-12 14:52:42 +0200

    Fix self join reference as per example https://guides.rubyonrails.org/association_basics.html#self-joins

--- db/migrate/20190221173404_create_questions.rb
+++ db/migrate/20190221173404_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.references :parent, foreign_key: true
+      t.references :parent, index: true
       t.references :question_type, foreign_key: true
 
       t.timestamps
