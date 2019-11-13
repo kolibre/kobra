cb2b9d6ccce2f4380a8ab8e029b6e8aae803b0da
Johan Abbors <jabbors@gmail.com>
2019-11-13 06:40:31 +0200

    Longer sleep before running a test case

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -10,7 +10,7 @@ do
     echo "running testcase ${testcase}"
     echo "starting container kobra:test ..."
     docker run -d --rm -e SECRET_KEY=testing -e STORAGE_PATH=/tmp -e DATABASE_URL=sqlite3:/usr/src/app/db/prod.sqlite3 --name kobratest -p 127.0.0.1:3000:3000 kobra:test
-    sleep 5
+    sleep 10
     frank -c ./testcases/${testcase}
     echo "cleaning up after test"
     echo "stopping container kobra:test ..."
